121820d1a980c3c7f2c2240a28c7866b0f40dfd1
mahyamohammadi <m.mohammadi.7994@gmail.com>
2026-08-05 17:06:58 +0400

    Update sales.js

--- sales.js
+++ sales.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ-NCetZ4D7QR-wv4JKhKMKM4JV7JkPeI54",
+  authDomain: "al-hudu-management.firebaseapp.com",
+  projectId: "al-hudu-management",
+  storageBucket: "al-hudu-management.firebasestorage.app",
+  messagingSenderId: "1045649803744",
+  appId: "1:1045649803744:web:bc6ead0755d196c020c385"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log("AL HUDU Firebase Connected");
